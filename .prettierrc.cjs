2c33626f12a06c6b23ffe248638eb5125bcc289f
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-03-26 09:31:33 -0700

    Update prettier config file
    (cherry picked from commit cc6ac55002fbe668ad7d35d36ed307798b8beec6)

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,5 +2,13 @@ module.exports = {
   plugins: [
     require.resolve('@shopify/prettier-plugin-liquid/standalone'),
     require.resolve('prettier-plugin-tailwindcss')
+  ],
+  overrides: [
+    {
+      files: '*.liquid',
+      options: {
+        parser: 'liquid-html'
+      }
+    }
   ]
 }
