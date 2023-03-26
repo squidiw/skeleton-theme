d74892dc9c4914a8ac316a9c11a2609632d0ffb3
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-03-25 20:49:30 -0700

    Install Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.liquid',
+    './frontend/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
